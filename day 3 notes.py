2267f1058b88c4f518b998f21fc4aa1619bd69f3
rahulmuthyala535 <124117686+rahulmuthyala535@users.noreply.github.com>
2023-02-01 18:34:36 +0530

    Add files via upload

---
+++ day 3 notes.py
@@ -0,0 +1,40 @@
+lists
+-----
+
+-> []
+-> In python the arrays concept are called as lists.
+-> In lists their are many operations to perform
+-> In list we have append,sort,count,remove,pop,len,reverse,index,clear
+-> 
+
+
+problems:
+
+-> create a list with 10 items ,print the elements 1 by 1
+
+-> create a list with 5 float numbers find and display sum and avg of the list
+
+-> after creating a list with 6 elements from the user extract only even numbers and print
+
+
+
+-> get list of numbers as input ,return the product if the product is less than 750 else retutrn sum
+
+
+
+tuple
+-----
+
+-> ()
+-> ordered
+-> unchangeble
+-> cant include items
+-> cant fetch using index
+
+
+
+dictionary
+----------
+
+-> {}
+-> dictionry contains elements with 2 units key and values,keys must be unique
